81bc5bb6e16a1183a8deaf9d28a49a3da9d640de
Marvellous-Chimaraoke <rokemarvellous@gmail.com>
2022-06-20 14:37:17 +0100

    tasks fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return (False)
+    except (TypeError, ValueError):
+        return False
     else:
-        return (True)
+        return True
